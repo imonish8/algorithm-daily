0653489a360ed4a7b3a7ce039e79b002ade5aa7c
Monish Nule <115737071+imonish8@users.noreply.github.com>
2024-03-12 00:26:43 +0530

    Create addTwo.js

---
+++ addTwo.js
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+         
+       int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; 
+      
+    }
+}  
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
