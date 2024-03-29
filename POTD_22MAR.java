7abb935a45b346f2f443a05002a59e9ca8ebd9eb
Monish Nule <115737071+imonish8@users.noreply.github.com>
2024-03-30 00:56:31 +0530

    Create POTD_22MAR.java

---
+++ POTD_22MAR.java
@@ -0,0 +1,30 @@
+//Given the head of a singly linked list, return true if it is a  palindrome or false otherwise.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+         List<Integer> list = new ArrayList();
+        while(head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        
+        int left = 0;
+        int right = list.size()-1;
+        while(left < right && list.get(left) == list.get(right)) {
+            left++;
+            right--;
+        }
+        return left >= right;
+    }
+    
+}
